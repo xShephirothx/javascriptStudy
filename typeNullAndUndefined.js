0eddedec56d632422101299c09df207ad1cc9c2a
Marcos Bento <marcos.x.marcos@hotmail.com>
2022-05-25 15:52:29 -0300

    exemplos aula 1

---
+++ typeNullAndUndefined.js
@@ -0,0 +1,20 @@
+//Tipos Null -> ausência de valor
+// Tipo Undefine -> undefined se refere ao valor de uma variável que não foi inicializada 
+
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+
+let inputs = null;
+let input2;
+
+console.log(inputs); // null
+console.log(input2); // undefined
+
+
