db34057e856439847a988276620b3528641644c1
Marcos Bento <marcos.x.marcos@hotmail.com>
2022-05-31 12:03:48 -0300

    exemplos aula 1

---
+++ variaveis.js
@@ -0,0 +1,45 @@
+// Existem 3 tipos de declarar variáveis em Javascript:
+
+// Var é uma delas.
+
+// var altura = 5;
+// var comprimento = 7;
+
+// var area = altura * comprimento;
+// console.log(area)
+
+// Let é a segunda.
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if (forma === "retangulo") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento)/2;
+}
+
+console.log(area)
+
+// Const é a terceira, e é constante.
+
+// CONVERSÃO DE TIPOS
+
+// Conversão implícita
+
+let numero = 5;
+let numeroString = '5';
+
+console.log(numero == numeroString)
+
+// 1 = atribuir valor a uma variável
+// 2 == comparando o valor/conteúdo da variável sem se atentar ao tipo
+// 3 === compara não só o valor mas também o tipo da variável 
+
+// Conversão explícita
+console.log(numero + Number(numeroString)) // Converte de string para "Number()"
+console.log(String(numero) + numeroString) // Converte de número para "String()"
+
+
