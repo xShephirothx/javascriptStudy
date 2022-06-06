9b7e5c7353ac1d340b3c21fbdf40c26546fc7f52
Marcos Bento <marcos.x.marcos@hotmail.com>
2022-06-06 15:06:12 -0300

    exemplos aula 1

---
+++ console.js
@@ -0,0 +1,12 @@
+// Entendendo sobre a método CONSOLE
+const minhaVar = true;
+
+console.clear()
+
+console.log(245)
+console.log("Eu sou um texto")
+console.log (minhaVar)
+
+// Serve pra ser impresso, pode ser impresso texto, número, valor de string, boolean, etc.
+
+console.error('Deu erro!')
---
+++ erros.js
@@ -0,0 +1,21 @@
+//Simulando os erros em JS
+
+const numero = 0;
+// const numero; - isso dá erro
+// variáveis do tipo Const não podem ser inicializadas sem valor.
+
+console.log(numero)
+// console.log(numero 
+// outro erro de Syntax, explicando que falta o símbolo de fecha parênteses.
+
+// console.log(minhaLet)
+// Erro de referência, a linguagem não conseguiu encontrar a variável;
+
+// TIPOS DE ERRO:
+
+// RangeError: Quando a variavei recebe um dado do tipo certo mas de formato não aceitável, ex: -1 para var maior que 0.
+// Reference Error: Quando o código tenta acessar algo que não existe (ex uma variável que não foi declarada naquele escopo).
+// Syntax Error;
+// Type Error: Indica que o código recebeu um dado diferente do que esperava, EX: Recebe String e aguardava Boolean.
+
+
