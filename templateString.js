b4570a9df096624a17947dd5aa539e590980e5f9
Marcos Bento <marcos.x.marcos@hotmail.com>
2022-07-07 19:56:16 -0300

    exemplo aula 1

---
+++ templateString.js
@@ -0,0 +1,14 @@
+// conseguimos concatenar strings utilizando o operador +
+
+const nome = 'Marcos';
+const idade = 2021-1994;
+const cidadeDeNascimento = 'Guarulhos'
+
+const apresentacao = 'meu nome é ' + nome + ' , minha idade é ' + idade + ' e, nasci na cidade de ' + cidadeDeNascimento;
+console.log(apresentacao)
+
+// Utilizando o Template String:
+
+const apresentacaoTemplateString = `meu nome é ${nome}, minha idade é ${idade}, e, nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacaoTemplateString)
