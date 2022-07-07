e80619a2d8828e6e1dbb0b6cacb7e3e6846e0924
Marcos Bento <marcos.x.marcos@hotmail.com>
2022-07-07 20:09:06 -0300

    Exemplo aula 1

--- templateString.js
+++ templateString.js
@@ -12,3 +12,44 @@ console.log(apresentacao)
 const apresentacaoTemplateString = `meu nome é ${nome}, minha idade é ${idade}, e, nasci na cidade de ${cidadeDeNascimento}`;
 
 console.log(apresentacaoTemplateString)
+
+// Praticando:
+// Dado as variáveis abaixo:
+
+a = []
+b = 20
+c = true 
+d = ''
+
+// Quais opções apresentam a saída "VERDADE"
+// Opção A:
+
+if ( b === 20 && c != false && d == 0) {
+    console.log('Verdade')
+} else {
+    console.log('Falso')
+}
+
+// Opção B:
+
+if (a == 0) {
+    console.log('Verdade')
+} else {
+    console.log('Falso')
+}
+
+// Opção C: // Único que dá FALSO
+
+if (d) {
+    console.log('Verdade')
+} else {
+    console.log('Falso')
+}
+
+// Opção D:
+
+if (b =='20') {
+    console.log('Verdade4')
+} else {
+    console.log('Falso')
+}
